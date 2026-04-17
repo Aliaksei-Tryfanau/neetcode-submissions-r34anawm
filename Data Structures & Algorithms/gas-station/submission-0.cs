896e39b1f11550ad909aa4823525a1b6316965fc
Aliaksei Tryfanau <35005922+Aliaksei-Tryfanau@users.noreply.github.com>
2026-04-17 13:16:30 -0400

    Add: gas-station - submission-0

---
+++ Data Structures & Algorithms/gas-station/submission-0.cs
@@ -0,0 +1,27 @@
+public class Solution 
+{
+    public int CanCompleteCircuit(int[] gas, int[] cost) 
+    {
+        //greedy
+        if (gas.Sum() < cost.Sum()) 
+        {
+            return -1;
+        }
+
+        int total = 0;
+        int res = 0;
+
+        for (int i = 0; i < gas.Length; i++) 
+        {
+            total += (gas[i] - cost[i]);
+
+            if (total < 0) 
+            {
+                total = 0;
+                res = i + 1;
+            }
+        }
+
+        return res;
+    }
+}

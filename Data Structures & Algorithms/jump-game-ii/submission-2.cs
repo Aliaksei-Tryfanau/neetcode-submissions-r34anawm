94b47d0cdd4b17dc5ef3aeb61198482fcf00c490
Aliaksei Tryfanau <35005922+Aliaksei-Tryfanau@users.noreply.github.com>
2026-05-03 10:25:45 -0400

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.cs
@@ -0,0 +1,23 @@
+public class Solution 
+{
+    public int Jump(int[] nums) 
+    {
+        //bottom up
+        int n = nums.Length;
+        int[] dp = new int[n];
+        Array.Fill(dp, 1000000);
+        dp[n - 1] = 0;
+
+        for (int i = n - 2; i >= 0; i--) 
+        {
+            int end = Math.Min(nums.Length, i + nums[i] + 1);
+
+            for (int j = i + 1; j < end; j++) 
+            {
+                dp[i] = Math.Min(dp[i], 1 + dp[j]);
+            }
+        }
+        
+        return dp[0];
+    }
+}

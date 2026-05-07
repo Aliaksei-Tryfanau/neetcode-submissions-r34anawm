2e70d77246027610ecfd397f2c5df40e3baa04ce
Aliaksei Tryfanau <35005922+Aliaksei-Tryfanau@users.noreply.github.com>
2026-05-07 12:58:14 -0400

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.cs
@@ -0,0 +1,48 @@
+public class TimeMap 
+{
+    private Dictionary<string, List<(int, string)>> keyStore;
+
+    public TimeMap() 
+    {
+        keyStore = new Dictionary<string, List<(int, string)>>();
+    }
+
+    public void Set(string key, string value, int timestamp) 
+    {
+        if (!keyStore.ContainsKey(key)) 
+        {
+            keyStore[key] = new List<(int, string)>();
+        }
+
+        keyStore[key].Add((timestamp, value));
+    }
+
+    public string Get(string key, int timestamp) 
+    {
+        if (!keyStore.ContainsKey(key)) 
+        {
+            return "";
+        }
+
+        var values = keyStore[key];
+        int left = 0, right = values.Count - 1;
+        string result = "";
+
+        while (left <= right) 
+        {
+            int mid = left + (right - left) / 2;
+
+            if (values[mid].Item1 <= timestamp) 
+            {
+                result = values[mid].Item2;
+                left = mid + 1;
+            } 
+            else 
+            {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}

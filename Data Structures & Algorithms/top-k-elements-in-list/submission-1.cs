a46ca1ad92a67e73818b1ef1b64ee395fdcf0291
Aliaksei Tryfanau <35005922+Aliaksei-Tryfanau@users.noreply.github.com>
2026-05-28 10:50:41 -0400

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) 
+    {
+        var count = new Dictionary<int, int>();
+        foreach (var num in nums) 
+        {
+            count[num] = count.GetValueOrDefault(num, 0) + 1;
+        }
+
+        var heap = new PriorityQueue<int, int>(); 
+
+        foreach (var entry in count) 
+        {
+            heap.Enqueue(entry.Key, entry.Value);
+            if (heap.Count > k) 
+            {
+                heap.Dequeue();
+            }
+        }
+
+        var res = new int[k];
+
+        for (int i = 0; i < k; i++) 
+        {
+            res[i] = heap.Dequeue();
+        }
+
+        return res;
+    }
+}

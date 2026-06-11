167837403c9aebb4b967dc99acf995ea6014f627
Aliaksei Tryfanau <35005922+Aliaksei-Tryfanau@users.noreply.github.com>
2026-06-11 11:01:29 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.cs
@@ -0,0 +1,41 @@
+public class Solution 
+{
+    public bool CheckInclusion(string s1, string s2) 
+    {
+        Dictionary<char, int> count1 = new Dictionary<char, int>();
+        foreach (char c in s1) 
+        {
+            count1[c] = count1.GetValueOrDefault(c, 0) + 1;
+        }
+
+        int need = count1.Count;
+        for (int i = 0; i < s2.Length; i++) 
+        {
+            Dictionary<char, int> count2 = new Dictionary<char, int>();
+            int cur = 0;
+
+            for (int j = i; j < s2.Length; j++) 
+            {
+                char c = s2[j];
+                count2[c] = count2.GetValueOrDefault(c, 0) + 1;
+
+                if (!count1.ContainsKey(c) || count1[c] < count2[c]) 
+                {
+                    break;
+                }
+
+                if (count1[c] == count2[c]) 
+                {
+                    cur++;
+                }
+
+                if (cur == need) 
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}

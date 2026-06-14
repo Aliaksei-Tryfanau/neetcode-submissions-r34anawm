653f5d2ab14f262142275d28b9fabfc6b4c93a8d
Aliaksei Tryfanau <35005922+Aliaksei-Tryfanau@users.noreply.github.com>
2026-06-14 10:44:07 -0400

    Add: longest-increasing-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-3.cs
@@ -0,0 +1,27 @@
+public class Solution 
+{
+    public int LengthOfLIS(int[] nums) 
+    {
+        int[] LIS = new int[nums.Length];
+        int maxLIS = 1;
+
+        for (int i = 0; i < LIS.Length; i++) 
+        {
+            LIS[i] = 1;
+        }
+
+        for (int i = nums.Length - 2; i >= 0; i--) 
+        {
+            for (int j = i + 1; j < nums.Length; j++) 
+            {
+                if (nums[i] < nums[j]) 
+                {
+                    LIS[i] = Math.Max(LIS[i], LIS[j] + 1);
+                    maxLIS = Math.Max(maxLIS, LIS[i]);
+                }
+            }
+        }
+        
+        return maxLIS;
+    }
+}

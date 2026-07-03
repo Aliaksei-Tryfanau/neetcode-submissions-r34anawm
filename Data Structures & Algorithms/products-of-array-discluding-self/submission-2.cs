77539ee2da0171ca14a4452fc26b16079737bb5a
Aliaksei Tryfanau <35005922+Aliaksei-Tryfanau@users.noreply.github.com>
2026-07-03 13:44:42 -0400

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.cs
@@ -0,0 +1,49 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) 
+    {
+        // int n = nums.Length;
+        // int[] res = new int[n];
+        // int[] pref = new int[n];
+        // int[] suff = new int[n];
+
+        // pref[0] = 1;
+        // suff[n - 1] = 1;
+
+        // for (int i = 1; i < n; i++) 
+        // {
+        //     pref[i] = nums[i - 1] * pref[i - 1];
+        // }
+
+        // for (int i = n - 2; i >= 0; i--) 
+        // {
+        //     suff[i] = nums[i + 1] * suff[i + 1];
+        // }
+
+        // for (int i = 0; i < n; i++) 
+        // {
+        //     res[i] = pref[i] * suff[i];
+        // }
+        
+        // return res;
+
+        int n = nums.Length;
+        int[] res = new int[n];
+        res[0] = 1;
+
+        for (int i = 1; i < n; i++) 
+        {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+
+        int postfix = 1;
+        
+        for (int i = n - 1; i >= 0; i--) 
+        {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+
+        return res;
+        //O(n)
+    }
+}
